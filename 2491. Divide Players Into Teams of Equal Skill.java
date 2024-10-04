7ecf2a678901d603134cc084cd1735bfa2913a7c
Souvik Sarkar <124427334+SouvikSarkar2@users.noreply.github.com>
2024-10-04 16:31:53 +0530

    Create 2491. Divide Players Into Teams of Equal Skill.java

---
+++ 2491. Divide Players Into Teams of Equal Skill.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public long dividePlayers(int[] skill) {
+        
+        Arrays.sort(skill);
+        
+        int n = skill.length;
+        int totalSkill = skill[0] + skill[n - 1]; 
+        long chemistrySum = 0;
+
+        
+        for (int i = 0; i < n / 2; i++) {
+            
+            if (skill[i] + skill[n - i - 1] != totalSkill) {
+                return -1;
+            }
+            System.out.println(skill[i]+" "+skill[n-1-i]);
+            
+            chemistrySum += (long) skill[i] * skill[n - i - 1];
+        }
+
+        return chemistrySum; 
+    }
+}
