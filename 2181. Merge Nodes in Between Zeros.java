b5b0c9e930dedaf6e8bedc5280f2bcf425524621
Souvik Sarkar <124427334+SouvikSarkar2@users.noreply.github.com>
2024-10-24 11:41:05 +0530

    Update 2181. Merge Nodes in Between Zeros.java

--- 2181. Merge Nodes in Between Zeros.java
+++ 2181. Merge Nodes in Between Zeros.java
@@ -1,3 +1,5 @@
+//BEATS 100 %
+
 class Solution {
     public ListNode mergeNodes(ListNode head)
     {
@@ -20,3 +22,49 @@ class Solution {
         return head.next;
     }
 }
+
+
+
+// BEATS 10%
+
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        //Find Sum And ADd the Sum in BEtween The Nodes
+
+        ListNode temp = head.next; 
+    ListNode prev = head;      
+    int sum = 0;
+
+    while (temp != null) {
+        if (temp.val == 0) { 
+            ListNode sumNode = new ListNode(sum); 
+            prev.next = sumNode;                  
+            sumNode.next = temp;
+            sum = 0;
+            prev = temp;
+        } else {
+            sum += temp.val; 
+        }
+        temp = temp.next; 
+    }
+
+        // Remove The Zeroes And return the nodes
+        ListNode reducer = head.next;
+        while(reducer!=null){
+            if(reducer.next.val==0){
+                reducer.next=reducer.next.next;
+            }
+            reducer=reducer.next;
+        }
+
+        return head.next;
+
+
+
+
+
+        
+    }
+}
+
+
