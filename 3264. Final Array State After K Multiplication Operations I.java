6357e905b444544f272a58196581daf0aa27c969
Souvik Sarkar <124427334+SouvikSarkar2@users.noreply.github.com>
2024-12-16 11:52:46 +0530

    Update 3264. Final Array State After K Multiplication Operations I.java

--- 3264. Final Array State After K Multiplication Operations I.java
+++ 3264. Final Array State After K Multiplication Operations I.java
@@ -16,3 +16,43 @@ class Solution {
         return nums;
     }
 }
+
+
+//O(nlogn)
+
+
+class Solution {
+
+    class Pair {
+    int value; 
+    int index; 
+
+   
+    public Pair(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+
+}
+
+
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
+            if (a.value != b.value) {
+                return Integer.compare(a.value, b.value); 
+            } else {
+                return Integer.compare(a.index, b.index); 
+            }
+        });
+       for(int i = 0 ; i<nums.length ; i++){
+        pq.add(new Pair(nums[i],i));
+       }
+       while(k>0){
+        Pair minPair = pq.poll();
+        nums[minPair.index]=minPair.value*multiplier;
+        pq.add(new Pair(minPair.value*multiplier,minPair.index));
+        k--;
+       }
+        return nums;
+    }
+}
