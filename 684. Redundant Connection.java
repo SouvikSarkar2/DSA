74282d8e8739ca143eed35bf733e9e388441b375
Souvik Sarkar <124427334+SouvikSarkar2@users.noreply.github.com>
2025-01-29 16:53:32 +0530

    Create 684. Redundant Connection.java

---
+++ 684. Redundant Connection.java
@@ -0,0 +1,41 @@
+class Solution {
+    private int[] parent;
+
+    
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    
+    private boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) {
+            return false; 
+        }
+        parent[rootX] = rootY;
+        return true;
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        parent = new int[n + 1];
+
+        
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        
+        for (int[] edge : edges) {
+            if (!union(edge[0], edge[1])) {
+                return edge;
+            }
+        }
+
+        return new int[0]; 
+    }
+}
